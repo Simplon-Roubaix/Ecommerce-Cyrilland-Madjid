99172ed7b50256e045ad966a4395edfb77a91a7b
madjidgo <madjidgo@gmail.com>
2017-09-06 16:23:16 +0200

    add script <script src=js/bootstrap.min.js></script>

--- index.php
+++ index.php
@@ -41,5 +41,6 @@
             r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
             ga('create','UA-XXXXX-X','auto');ga('send','pageview');
         </script>
+        <script src="js/bootstrap.min.js"></script>  
     </body>
 </html>
