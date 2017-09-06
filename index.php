279174fb38d648b947b9dcbf1993f614ede4c824
madjidgo <madjidgo@gmail.com>
2017-09-06 16:36:31 +0200

    create page header.php footer.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title></title>
+        <title>Site Ecommerce</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,18 +15,26 @@
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
     <body>
+      <!--  start header -->
       <?php
-        include header.php
+        include "header.php";
        ?>
+       <!-- end header -->
+
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
         <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <!-- start body -->
+        <p>Hello world! This is HTML5 Boilerplate.</p>dy
+        <!-- end body -->
+
+         <!-- start footer -->
         <?php
-          include footer.php
+          include "footer.php";
          ?>
+         <!-- end footer -->
         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
@@ -41,6 +49,6 @@
             r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
             ga('create','UA-XXXXX-X','auto');ga('send','pageview');
         </script>
-        <script src="js/bootstrap.min.js"></script>  
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
