869c840bf5704e6b29f2e81b59b5ff0c5d6b442f
madjidgo <madjidgo@gmail.com>
2017-09-06 16:42:47 +0200

    get in shape page  footer.php 	modifié :         header.php 	modifié :         index.php

---
+++ header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Site Ecommerce</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+      <header>
+    <h1>Titre commun à tout le site</h1>
+    <nav>
+      <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
