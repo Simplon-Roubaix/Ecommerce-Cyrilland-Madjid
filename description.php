09a74cc7d6e3e9a25ad5312b024a3755a4467863
madjidgo <madjidgo@gmail.com>
2017-09-07 18:19:08 +0200

    modification description.php

--- description.php
+++ description.php
@@ -2,9 +2,12 @@
 
 <?php
 $description = array(
-  array('img' =>'capote.jpg','titre'=>'preservatif', 'description' => 'Rougir de Plaisir', 'alt' => "preservatif"),
- array('img' =>'préservatifs2.jpg' ,'titre' => 'preservatifcolor' , 'description' => 'l\envie'),
- array('img' =>' capote.jpg', 'titre' => 'le bon reflex', 'description' => 'La Vie'),
+  array('img' =>'capote.jpeg','titre'=>'Oui Oui', 'description' => 'Rougir de Plaisir', 'alt' => "preservatif"),
+ array('img' =>'capote1.jpeg' ,'titre' => 'Non Non' , 'description' => 'Rougir ou Mourir'),
+ array('img' =>' capote2.jpeg', 'titre' => 'Ou Ou', 'description' => 'Rougir ou Partir'),
+ array('img' =>' capote4.jpeg', 'titre' => 'Ici Ici', 'description' => 'Rougir ou Dormir'),
+ array('img' =>' capote5.jpeg', 'titre' => 'Hy Hy', 'description' => 'Rougir ou Détruire'),
+ array('img' =>' capote6.jpeg', 'titre' => 'Go Go', 'description' => 'Rougir ou Vomir '),
 );
 
 
