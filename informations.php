692d9cd98b2488bcac3ae00bb06d4bc08e517765
madjidgo <madjidgo@gmail.com>
2017-09-08 14:17:02 +0200

    get in shape informtions.php

--- informations.php
+++ informations.php
@@ -1,6 +1,6 @@
 <?php
 $info  = array('h1' => 'Site de Ecommerce',
 'description' => 'Performés avec notre sélection de produits dédiés à la prouesse sexuelle!',
-'auteur' => ['Cyril Forrez', 'Mad le fou',' Copyright.fr -2017-Simplon-Roubaix'],
+'auteur' => ['Cyril Forrez', 'Madjid Goumidi',' Copyright.fr -2017-Simplon-Roubaix'],
  );
  ?>
