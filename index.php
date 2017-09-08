7cb16bd5052ed3af6b3765bc2d6391827ced942b
cyrilforrez <cyilforrez@gmail.com>
2017-09-08 14:28:17 +0200

    change name buttons

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
                 <h4 class="card-title"> <?php   echo $value['titre']; ?></h4>
                   <p class="card-text"><?php echo $value['description']; ?></p>
 
-                  <a href="infoArticle.php?titre=<?php echo $value['titre']; ?>&amp;img=<?php echo $value['img']; ?>&amp;description=<?php echo $value['description']; ?>" class="btn btn-danger">Clito</a>
+                  <a href="infoArticle.php?titre=<?php echo $value['titre']; ?>&amp;img=<?php echo $value['img']; ?>&amp;description=<?php echo $value['description']; ?>" class="btn btn-danger">Apperçue</a>
             </div>
       </div>
 <?php
