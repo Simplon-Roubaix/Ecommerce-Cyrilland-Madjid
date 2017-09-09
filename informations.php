98bb03eac5b21c2c41cdcc039ddfd02d98f60218
cyrilforrez <cyilforrez@gmail.com>
2017-09-09 23:29:44 +0200

    modif auteur and h1

--- informations.php
+++ informations.php
@@ -1,6 +1,6 @@
 <?php
-$info  = array('h1' => 'Ecommerce Gaming',
+$info  = array('h1' => 'E-Gaming',
 'description' => 'Site de ecommerce pour les gameurs et les gameuses !',
-'auteur' => ['Cyril Forrez', 'Mad le fou',' Copyright.fr -2017-Simplon-Roubaix'],
+'auteur' => ['Oxawa Cyril', 'Mad le fou',' Copyright.fr -2017-Simplon-Roubaix'],
  );
  ?>
