3857d5c7ae86a19e34ab6481ad42cd0b7dd0a586
cyrilforrez <cyilforrez@gmail.com>
2017-09-09 23:25:56 +0200

    add file navbar

---
+++ navbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar sticky-top navbar-light bg-faded navbar navbar-toggleable-md navbar-light bg-faded" style="background-color: #e3f2fd;">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="http://localhost/Ecommerce-Cyrilland-Madjid/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Liste des jeux
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="http://localhost/Ecommerce-Cyrilland-Madjid/infoArticle.php?article=0">Rainbow 6 siege</a>
+    <a class="dropdown-item" href="http://localhost/Ecommerce-Cyrilland-Madjid/infoArticle.php?article=1"> Call Of Duty WWII</a>
+    <a class="dropdown-item" href="http://localhost/Ecommerce-Cyrilland-Madjid/infoArticle.php?article=2">Dishonored</a>
+    <a class="dropdown-item" href="http://localhost/Ecommerce-Cyrilland-Madjid/infoArticle.php?article=3">For Honor</a>
+    <a class="dropdown-item" href="http://localhost/Ecommerce-Cyrilland-Madjid/infoArticle.php?article=4">  Sniper Ghost Warrior 3</a>
+    <a class="dropdown-item" href="http://localhost/Ecommerce-Cyrilland-Madjid/infoArticle.php?article=5">Valkyria Revolution</a>
+  </div>
+</div>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
