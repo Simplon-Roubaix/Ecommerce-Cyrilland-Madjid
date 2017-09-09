2e3736985b7c4082a6f39ac2a99c8e9d0b092190
cyrilforrez <cyilforrez@gmail.com>
2017-09-09 23:28:06 +0200

    modif title of site

--- header.php
+++ header.php
@@ -6,7 +6,7 @@ require "informations.php";
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Ecommerce Gaming</title>
+        <title>E-Gaming</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
