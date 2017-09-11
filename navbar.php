1d15a73aed93c37f5c455bcc26076cbe30822fe3
cyrilforrez <cyilforrez@gmail.com>
2017-09-11 10:58:23 +0200

    modif cation to form

--- navbar.php
+++ navbar.php
@@ -22,8 +22,8 @@
   </div>
 </div>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+    <form class="form-inline my-2 my-lg-0" action="search.php" method="post">
+      <input class="form-control mr-sm-2" name="form" type="text" placeholder="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
