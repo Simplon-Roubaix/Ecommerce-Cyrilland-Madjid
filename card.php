b455a0185bee7df673fccb3e7a266b350a22c2db
cyrilforrez <cyilforrez@gmail.com>
2017-09-11 10:57:20 +0200

    create and add card.php

---
+++ card.php
@@ -0,0 +1,22 @@
+<article class="card">
+  	<h1><?php echo $description[$article]['titre']; ?></h1>
+
+
+  		<div class="container">
+			<div class="row">
+
+  				<div class="col-lg-4">
+  					<img class="card-img-top " src="<?php echo $description[$article]['img'];?>" alt="Jeux">
+  				</div>
+
+  				<div class="col-lg-8">
+  					<div class="card-block ">
+    					<p class="card-text ">
+                <?php echo $description[$article]['jeux'];?>
+              </p>
+  					</div>
+  				</div>
+
+  			</div>
+  		</div>
+</article>
